bc57841a6ea7ca8cb1db320c591c2e1ba9ce4978
Lee Mason <contact@nohalfpixels.com>
2014-07-14 13:12:14 +0100

    updating pages branch

--- build_docs.sh
+++
@@ -1,2 +0,0 @@
-rm -rf /var/www/fluent-framework.com/pages/docs/* &&
-php "/var/www/fluent-framework.com/wp-content/plugins/fluent-framework/node_modules/grunt-phpdocumentor/bin/phpDocumentor.phar" --target="/var/www/fluent-framework.com/pages/docs/" --directory="/var/www/fluent-framework.com/wp-content/plugins/fluent-framework/" --ignore="node_modules/*"
